ead4a9b659cc74b9862e2465152539b294a1f807
Mohamed Saed <mohojr2003@gmail.com>
2025-08-01 21:33:03 +0300

    Add files via upload

---
+++ my projects/script.js
@@ -0,0 +1,8 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function toggleMenu() {
+  const nav = document.querySelector('.nav-links');
+  nav.classList.toggle('show');
+}
