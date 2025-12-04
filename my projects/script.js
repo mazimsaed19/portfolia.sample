bca43f0115eda2492068b3ecd8f1dbbaf3f69556
Mohamed Saed <mohojr2003@gmail.com>
2025-12-04 13:59:26 +0300

    Delete my projects directory

--- my projects/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
-
-function toggleMenu() {
-  const nav = document.querySelector('.nav-links');
-  nav.classList.toggle('show');
-}
